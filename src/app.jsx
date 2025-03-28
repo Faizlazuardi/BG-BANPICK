8f044933af9f968d8edb670cf79fffe4db279ea1
Faizlazuardi <faizlazuardi85@gmail.com>
2025-03-28 21:44:44 +0700

    fix sidebar

--- src/app.jsx
+++ src/app.jsx
@@ -3,28 +3,16 @@ import SideBar from "./components/SideBar";
 import Dashboard from "./pages/Dashboard";
 import DraftPick from "./pages/DraftPick";
 
-function App() {
+export default function App() {
     return (
         <Router>
-            <MainContent />
+            <div className="flex select-none">
+                <SideBar/>
+                <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/draftpick" element={<DraftPick />} />
+                </Routes>
+            </div>
         </Router>
     );
 }
-
-function MainContent() {
-    const location = useLocation();
-
-    const showSidebar = location.pathname === "/" || location.pathname === "/draftpick";
-
-    return (
-        <div className="flex select-none">
-            {showSidebar && <SideBar />}
-            <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/draftpick" element={<DraftPick />} />
-            </Routes>
-        </div>
-    );
-}
-
-export default App;
